e4083569d4516ede2abbc194a95b14717e1d50e6
Marks Polakovs <marks@markspolakovs.me>
2020-10-12 17:55:25 +0100

    fixup

--- update.sh
+++ update.sh
@@ -12,9 +12,20 @@ if [ "$data" != "$olddata" ]; then
     # we have a new record
     date=$(date +%Y-%m-%d)
     echo "$date,$data" >> york-uni-covid.csv
-    git checkout -b "auto-$date"
+
+    branch="auto-$date"
+    # Test if the branch exists
+    git rev-parse --verify "$branch" > /dev/null 2>&1
+    if $?; then
+        echo "Warning: $branch already exists"
+        git checkout "$branch"
+    else
+        git checkoug -b "$branch"
+    fi
+    
+    git checkout -b "$branch"
     git add york-uni-covid.csv
     git commit -m "Add data for $date"
-    git push origin "auto-$date"
-    gh pr create --title "Add UoY date for $date"
+    git push origin "$branch"
+    gh pr create --head --title "Add UoY date for $date" --reviewers markspolakovs --body "This PR is automatically generated."
 fi
